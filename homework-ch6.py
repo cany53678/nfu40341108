0debf8de9c5955e36b89b1c238b52febd76d8630
cany53678 <40341108@gm.nfu.edu.tw>
2017-04-06 17:21:00 +0800

    0406

--- homework-ch6.py
+++ homework-ch6.py
@@ -30,9 +30,9 @@ fig, ax = plt.subplots(1, 1)
 
 x = np.linspace(norm.ppf(0.01),norm.ppf(0.99), 100)
 
-ax.plot(x, norm.pdf(x),'r-', lw=5, alpha=0.6, label='norm pdf')
+ax.plot(x, norm.pdf(x),'ro', lw=5, alpha=0.6, label='norm pdf')
 
-ax.plot(x, norm.pdf(x), 'k-', lw=2, label='frozen pdf')
+ax.plot(x, norm.pdf(x), 'k-o', lw=2, label='frozen pdf')
 
 r = norm.rvs(size=1000)
 
